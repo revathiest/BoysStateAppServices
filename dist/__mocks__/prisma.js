9ab70a91d10dbafa8153145fb114aa0a254584de
revathiest <revathiest@gmail.com>
2025-06-22 22:29:35 -0500

    Add user registration and login endpoints

--- dist/__mocks__/prisma.js
+++ dist/__mocks__/prisma.js
@@ -2,5 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const prisma = {
     $queryRaw: jest.fn(),
+    user: {
+        create: jest.fn(),
+        findUnique: jest.fn(),
+    },
 };
 exports.default = prisma;
