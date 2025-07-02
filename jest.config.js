ab206b1f37ebfd15eb367e56cc29c685b5a36c95
revathiest <revathiest@gmail.com>
2025-07-02 06:45:03 -0500

    Updated jest.confog.js and file structure

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,38 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  roots: ['<rootDir>/__tests__'],
+  moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
+  moduleDirectories: ['node_modules', '<rootDir>'],
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  },
+
+  // === COVERAGE SETTINGS ===
+  collectCoverage: true,                        // always gather coverage
+  coverageDirectory: 'coverage',               // output folder
+  coverageReporters: ['text', 'lcov'],         // text = console table, lcov for HTML
+  collectCoverageFrom: [
+    'src/**/*.{js,ts}',
+    'prisma/**/*.{js,ts}',
+    'botactions/**/*.{js,ts}',
+    'jobs/**/*.{js,ts}',
+    'models/**/*.{js,ts}',
+    '!**/node_modules/**',
+    '!**/__mocks__/**'
+  ],
+    "coverageThreshold": {
+    "global": {
+      "branches": 80,
+      "functions": 80,
+      "lines": 80,
+      "statements": 80
+    }
+  },
+
+  // === SILENT MODE ===
+  verbose: false,
+  silent: true,
+  reporters: ['default']
 };
