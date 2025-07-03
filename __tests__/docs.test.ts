2edfa13ac405aae54530f5b949be0954611f060a
revathiest <revathiest@gmail.com>
2025-07-03 07:19:41 -0500

    Document Swagger download link

--- __tests__/docs.test.ts
+++ __tests__/docs.test.ts
@@ -14,6 +14,17 @@ describe('GET /docs', () => {
     expect(res.status).toBe(200);
     expect(res.body.openapi).toBe('3.0.0');
   });
+
+  it('contains a download link for swagger.json', async () => {
+    const res = await request(app).get('/docs/');
+    expect(res.text).toMatch('swagger-ui-custom.js');
+  });
+
+  it('serves the custom javascript', async () => {
+    const res = await request(app).get('/docs/swagger-ui-custom.js');
+    expect(res.status).toBe(200);
+    expect(res.text).toMatch('Download swagger.json');
+  });
     
   it('uses a local server url when not in production', () => {
     expect(swaggerDoc.servers[0].url).toMatch('http://localhost');
